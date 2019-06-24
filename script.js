aba34e8202249485d3d7f4b9004bfb1f525e7851
Gabyong <gabyongece@gmail.com>
2019-06-24 14:21:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ if($("#fname")[0].checkValidity() && $("#lname")[0].checkValidity() &&
       $("#email")[0].checkValidity() && $("#phone")[0].checkValidity())
     {
       $.ajax({
-        type:"POST", url:"https://stage.gototaia.com/Gabyong/ping.php", data: dataString,
+        type:"POST", url:"https://gabyong.github.io/TAIA/ping.php", data: dataString,
         success: function(data){
           alert(data);
           }
